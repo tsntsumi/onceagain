b643adaaf91837ca5eddbbac6235639106af6df5
tsntsumi <tsutsumi.kikuo@gmail.com>
2025-03-04 06:07:42 +0800

    Update menu title

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -3,31 +3,31 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
+    title: "ホーム",
     path: "/",
     newTab: false,
   },
   {
     id: 2,
-    title: "About",
+    title: "私達について",
     path: "/about",
     newTab: false,
   },
   {
     id: 3,
-    title: "Pricing",
+    title: "料金",
     path: "/pricing",
     newTab: false,
   },
   {
     id: 4,
-    title: "Contact",
+    title: "問合せ",
     path: "/contact",
     newTab: false,
   },
   {
     id: 5,
-    title: "Blog",
+    title: "ブログ",
     path: "/blogs",
     newTab: false,
   },
