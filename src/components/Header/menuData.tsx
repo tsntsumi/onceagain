f5d93944af994551a3e83866443424225c0890a6
tsntsumi <tsutsumi.kikuo@gmail.com>
2025-03-03 10:31:03 +0800

    Fix menu word
    If the words in the menu item are Japanese rather than English words,
    the characters will be broken down and vertically They were lined
    up. The method of specifying whitespace-nowrap was also correct, but I
    changed it to English vocabulary to arrange the header menu in a
    compact way.

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -9,76 +9,27 @@ const menuData: Menu[] = [
   },
   {
     id: 2,
-    title: "私たちについて",
+    title: "About",
     path: "/about",
     newTab: false,
   },
   {
     id: 3,
-    title: "サービス",
+    title: "Features",
     path: "/pricing",
     newTab: false,
   },
   {
-    id: 5,
-    title: "コンタクト",
+    id: 4,
+    title: "Contact",
     path: "/contact",
     newTab: false,
   },
   {
     id: 5,
-    title: "ブログ",
+    title: "Blog",
     path: "/blogs",
     newTab: false,
-  },
-  {
-    id: 6,
-    title: "ページ",
-    newTab: false,
-    submenu: [
-      {
-        id: 61,
-        title: "私たちについて",
-        path: "/about",
-        newTab: false,
-      },
-      {
-        id: 62,
-        title: "サービス一覧",
-        path: "/pricing",
-        newTab: false,
-      },
-      {
-        id: 63,
-        title: "コンタクト",
-        path: "/contact",
-        newTab: false,
-      },
-      {
-        id: 64,
-        title: "ブログ",
-        path: "/blogs",
-        newTab: false,
-      },
-      {
-        id: 66,
-        title: "サインアップ",
-        path: "/signup",
-        newTab: false,
-      },
-      {
-        id: 67,
-        title: "サインイン",
-        path: "/signin",
-        newTab: false,
-      },
-      /* {
-       *   id: 68,
-       *   title: "エラーページ",
-       *   path: "/error",
-       *   newTab: false,
-       * }, */
-    ],
   },
 ];
 export default menuData;
