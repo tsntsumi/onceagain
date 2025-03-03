968632f61db2bf53f73e254cd9f2c90d46ecdb52
tsntsumi <tsutsumi.kikuo@gmail.com>
2025-03-04 06:08:44 +0800

    Update price table

--- src/stripe/pricingData.ts
+++ src/stripe/pricingData.ts
@@ -2,61 +2,60 @@ import { Price } from "@/types/price";
 
 export const pricingData: Price[] = [
   {
-    id: "price_1NQk5TLtGdPVhGLecVfQ7mn0",
+    id: "price_1QyYgDFI0fllSO6vjiduCl1W",
     unit_amount: 0,
-    actual_amount: 800000,
+    actual_amount: 8000 * 100,
     category: "セミナー",
     nickname: "裏口からの AI 起業入門",
     trial: 0,
-    term: "Lifetime",
+    guarantee: 0,
+    subscription: "Lifetime",
     recomend: "おすすめ",
     offers: [
-      "ブラックからの卒業！キャリア再生の道への扉を開けて、" +
-      "年収600万以上の高収入で余裕のある生活を送る、" +
-      "6ヶ月後の未来のあなたに出会う、",
+      "ブラックからの卒業！キャリア再生の道への扉を開けて、年収600万以上の高収入で余裕のある生活を送る、6ヶ月後の未来のあなたに出会う、",
       "家族を大切にする地方在住プログラマのための AI 起業セミナー",
-      "仕事で不遇な状況にある、地方在住の家族を大切にするプログラマでも、半年で 年収 600 万円と、家族のための時間を確保できる AI 起業を解説します。",
+      "仕事で不当な扱いを受けている地方在住の家族を大切にするプログラマが、半年で年収 600 万円と、家族のための時間を確保できる AI 起業を解説します。",
     ],
     cta: "今すぐ席を確保"
   },
   {
-    id: "price_1NQk5TLtGdPVhGLecVfQ7mn0",
-    unit_amount: 498000,
-    actual_amount: 980000,
+    id: "price_1QyYpzFI0fllSO6vNeYZAGE8",
+    unit_amount: 49800 * 100,
+    actual_amount: 99800 * 100,
     actual: " / 月",
     category: "AIオートメーション先行予約",
     nickname: "AI チャットボット",
     trial: 0,
-    term: "Lifetime",
+    guarantee: 0,
+    subscription: "Lifetime",
     recomend: "いち推し",
     offers: [
-      "チャットボットで時短・効率化。\
-      ホームページやECサイトで顧客問い合わせシステム、\
-      社内ナレッジベースシステムを構築できます。",
+      "チャットボットで時短・効率化。ホームページやECサイトで顧客問い合わせシステム、社内ナレッジベースシステムを構築できます。",
       "あなたのビジネス専用に、PDFを読み込ませるだけで AI をカスタマイズ。",
-      "事前予約で、約半額の 4,980 円を1度支払うだけで、月額無料になります。",
-      "応募締切は、3月31日。4月30日からご利用いただけます。",
+      "💴今なら事前予約で3ヶ月分前払いいただくと、月々9800円が永久無料。",
+      "応募締切は、3月31日。遅くとも4月30日からご利用いただけます。",
     ],
     cta: "今すぐ予約"
   },
   {
-    id: "price_1NQk5TLtGdPVhGLecVfQ7mn0",
-    unit_amount: 198000,
-    actual_amount: 66000,
+    id: "price_1QyYxtFI0fllSO6vGGkWeWev",
+    unit_amount: 1980 * 100,
+    actual_amount: 660 * 100,
     actual: " / 月",
     category: "AIオートメーション先行予約",
     nickname: "インスタ投稿ボット",
     trial: 0,
-    term: "Lifetime",
-    recomend: "",
+    guarantee: 30,
+    subscription: "Lifetime",
+    recomend: "一番人気",
     offers: [
       "SNS への投稿は、毎日決まった時間に行うのがキモ。\
-      バズるコンテンツを創らなくても地道な投稿が評価されます。",
+       バズるコンテンツを創らなくても地道な投稿が評価されます。",
       "忙しくて投稿を創る暇も投稿する時間もないあなたのために、AIが代行します。",
       "インスタ投稿ボットは、スプレッドシートに用意した投稿テーマから、\
-      AIがコンテンツを自動生成して、決まった時間に投稿します。",
-      "事前予約で最初に3ヶ月分の1,980円を支払うだけで、月々660円が月額無料になります。",
-      "応募締切は、3月31日。4月30日からご利用いただけます。",
+       AIがコンテンツを自動生成して、決まった時間に投稿します。",
+      "💰今なら事前予約で3ヶ月分1,980円前払いいただくと、月々660円が永久無料。",
+      "応募締切は、3月31日。遅くとも4月30日からご利用いただけます。",
     ],
     cta: "今すぐ予約"
   },
