e44e8400701635dd39775fbfa8949c01eb674376
tsntsumi <tsutsumi.kikuo@gmail.com>
2025-03-03 20:38:01 +0800

    Update for integration according to the documentation

---
+++ integration.config.tsx
@@ -0,0 +1,13 @@
+const integrations = {
+  isSanityEnabled: false,
+  isStripeEnabled: false,
+  isAlgoliaEnabled: false,
+  isMailchimpEnabled: false,
+  isAuthEnabled: true,
+}
+
+const messages = [
+  "",
+]
+
+export { integrations, messages }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    theme: {
+      container: true,
+      padding: "1rem",
+    },
     extend: {
       colors: {
-        // ...colors,
         primary: "#48A860", // Chateau Green
-        secondary: "#98FB98", // Mint Green
         // primary: "#1A202C",
+        secondary: "#98FB98", // Mint Green
+        //current: "currentColor",
+        //transparent: "transparent",
+        //white: "#FFFFFF",
+        //black: "#090E34",
+        //dark: "#1D2144",
+        //primary: "#4A6CF7",
+        //yellow: "#FBB840",
+        "body-color": "#959CB1",
       },
     },
   },
