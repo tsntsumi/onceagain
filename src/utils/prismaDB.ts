d859257cba8a322245c7abc246d5dda899de8e60
tsntsumi <tsutsumi.kikuo@gmail.com>
2025-03-04 17:49:20 +0800

    Fix prisma client did not initialized

--- src/utils/prismaDB.ts
+++ src/utils/prismaDB.ts
@@ -1,22 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export let prisma: PrismaClient;
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma =   new PrismaClient({
+if (!global.prisma) {
+  global.prisma = new PrismaClient({
     log: ["query"],
-  });
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      log: ["query"],
-    })
-  }
-  prisma = global.prisma;
+  })
 }
 
+export const prisma = global.prisma;
 export default prisma;
